36be3faaa56508aea51622b20eea251dffb94d79
Coty Condry <ccondry@cisco.com>
2019-03-15 09:21:36 -0500

    make this library a class

--- test/test.js
+++ test/test.js
@@ -1,11 +1,17 @@
-const lib = require('../src')
+const client = require('../src')
+
+const uccx = new client({
+  url: 'https://uccx1.dcloud.cisco.com/adminapi',
+  username: 'administrator',
+  password: 'C1sco12345'
+})
 
 // config values for tests
 const resourceId = 'sjeffers'
 
-describe('lib.listResource()', function () {
+describe('uccx.listResource()', function () {
   it('should get list of resources (agents)', function (done) {
-    lib.listResource({})
+    uccx.listResource({})
     .then(response => {
       console.log('found', response.length, 'items')
       done()
@@ -16,9 +22,9 @@ describe('lib.listResource()', function () {
   })
 })
 
-describe('lib.listResource({csqId, withCsqs, lastReSkillDetails})', function () {
+describe('uccx.listResource({csqId, withCsqs, lastReSkillDetails})', function () {
   it('should get list of resources (agents) by csqId = 1, with CSQ map, and with last reskill info', function (done) {
-    lib.listResource({csqId: 1, withCsqs: true, lastReSkillDetails: true})
+    uccx.listResource({csqId: 1, withCsqs: true, lastReSkillDetails: true})
     .then(response => {
       console.log('found', response.length, 'items')
       done()
@@ -29,15 +35,15 @@ describe('lib.listResource({csqId, withCsqs, lastReSkillDetails})', function () 
   })
 })
 
-// describe('lib.listResource(csqId)', function () {
-//   it('should get single resource by ID', function (done) {
-//     lib.getResource(resourceId)
-//     .then(response => {
-//       console.log('found', response.alias)
-//       done()
-//     })
-//     .catch(e => {
-//       done(e)
-//     })
-//   })
-// })
+describe('uccx.getResource()', function () {
+  it('should get single resource by ID', function (done) {
+    uccx.getResource(resourceId)
+    .then(response => {
+      console.log('found', response.alias)
+      done()
+    })
+    .catch(e => {
+      done(e)
+    })
+  })
+})
