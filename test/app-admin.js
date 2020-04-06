bf1a97cd06a3a43cc59d5e3e5de6f3b57aa810f9
Coty Condry <ccondry@cisco.com>
2020-04-06 11:49:17 -0500

    add tests

--- test/app-admin.js
+++ test/app-admin.js
@@ -38,41 +38,75 @@ const uccx = new client({
 //   })
 // })
 
-describe('uccx.appAdmin.chatWidget.list()', function () {
-  it('should list chat widgets using appAdmin web UI', function (done) {
-    uccx.appAdmin.chatWidget.list()
-    // uccx.appAdmin.listChatWidgets()
-    .then(response => {
-      // console.log(JSON.stringify(response, null, 2))
-      console.log('found', response.length, 'widgets')
-      done()
-    })
-    .catch(e => {
-      done(e)
-    })
-  })
-})
+// describe('uccx.appAdmin.chatWidget.list()', function () {
+//   it('should list chat widgets using appAdmin web UI', function (done) {
+//     uccx.appAdmin.chatWidget.list()
+//     // uccx.appAdmin.listChatWidgets()
+//     .then(response => {
+//       // console.log(JSON.stringify(response, null, 2))
+//       console.log('found', response.length, 'widgets')
+//       done()
+//     })
+//     .catch(e => {
+//       done(e)
+//     })
+//   })
+// })
 
-describe('uccx.appAdmin.chatWidget.delete()', function () {
-  it('should delete a chat widget using appAdmin web UI', function (done) {
-    uccx.appAdmin.chatWidget.delete('82')
-    .then(response => {
-      // console.log(JSON.stringify(response, null, 2))
-      done()
-    })
-    .catch(e => {
-      done(e)
-    })
-  })
-})
+// let id
+// describe('uccx.appAdmin.chatWidget.getCsqRefUrl()', function () {
+//   it('should get CSQ REF URL', function (done) {
+//     uccx.appAdmin.getCsqRefUrl('Chat_0325')
+//     .then(response => {
+//       console.log(response)
+//       id = response.split('/').pop()
+//       done()
+//     })
+//     .catch(e => {
+//       done(e)
+//     })
+//   })
+// })
 
-describe('uccx.appAdmin.chatWidget.list()', function () {
-  it('should list chat widgets using appAdmin web UI', function (done) {
-    uccx.appAdmin.chatWidget.list()
-    // uccx.appAdmin.listChatWidgets()
+// describe('uccx.appAdmin.chatWidget.delete()', function () {
+//   it('should delete a chat widget using appAdmin web UI', function (done) {
+//     uccx.appAdmin.chatWidget.delete(id)
+//     .then(response => {
+//       // console.log(JSON.stringify(response, null, 2))
+//       done()
+//     })
+//     .catch(e => {
+//       done(e)
+//     })
+//   })
+// })
+
+describe('uccx.appAdmin.createChatOrEmailCsq()', function () {
+  it('should create a chat widget using appAdmin web UI', function (done) {
+    uccx.appAdmin.createChatOrEmailCsq({
+      "name": "Chat_4461",
+      "queueType": "CHAT",
+      "queueAlgorithm": "FIFO",
+      "routingType": "INTERACTIVE",
+      "resourcePoolType": "SKILL_GROUP",
+      "poolSpecificInfo": {
+        "skillGroup": {
+          "skillCompetency": [
+            {
+              "competencelevel": 5,
+              "skillNameUriPair": {
+                "@name": "Chat_4461",
+                "refURL": "https://uccx1.dcloud.cisco.com/adminapi/skill/431"
+              },
+              "weight": 1
+            }
+          ],
+          "selectionCriteria": "Longest Available"
+        }
+      }
+    })
     .then(response => {
       // console.log(JSON.stringify(response, null, 2))
-      console.log('found', response.length, 'widgets')
       done()
     })
     .catch(e => {
--- test/csq.js
+++ test/csq.js
@@ -1,6 +1,10 @@
 const client = require('../src')
-const credentials = require('./credentials')
-const uccx = new client(credentials)
+require('dotenv').config()
+const uccx = new client({
+  url: process.env.URL,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD
+})
 
 const userId = '0325'
 
@@ -14,53 +18,56 @@ const cache = {
 /*********************
 Contact Service Queues
 *********************/
-describe('uccx.csq.create(body)', function () {
-  it('should create csq', function (done) {
-    uccx.csq.create({
-      name: 'test12345test',
-      queueType: 'VOICE',
-      routingType: 'VOICE',
-      queueAlgorithm: 'FIFO',
-      autoWork: true,
-      wrapupTime: 1,
-      resourcePoolType: 'SKILL_GROUP',
-      serviceLevel: 5,
-      serviceLevelPercentage: 70,
-      poolSpecificInfo: {
-        skillGroup: {
-          skillCompetency: [{
-            competencelevel: 5,
-            skillNameUriPair:{
-              '@name': cache.skillName,
-              refURL: cache.skillRefUrl
-            },
-            weight: 1,
-          }],
-          selectionCriteria: 'Longest Available'
-        }
-      },
-      prompt: {
-        '@name': 'mainMenu.wav',
-        refURL: 'https://uccx1.dcloud.cisco.com/adminapi/prompt/mainMenu.wav'
-      }
-    })
-    .then(refURL => {
-      console.log('created csq:', refURL)
-      // extract csq ID
-      cache.csqId = refURL.split('/').pop()
-      done()
-    })
-    .catch(e => {
-      done(e)
-    })
-  })
-})
+// describe('uccx.csq.create(body)', function () {
+//   it('should create csq', function (done) {
+//     uccx.csq.create({
+//       name: 'test12345test',
+//       queueType: 'VOICE',
+//       routingType: 'VOICE',
+//       queueAlgorithm: 'FIFO',
+//       autoWork: true,
+//       wrapupTime: 1,
+//       resourcePoolType: 'SKILL_GROUP',
+//       serviceLevel: 5,
+//       serviceLevelPercentage: 70,
+//       poolSpecificInfo: {
+//         skillGroup: {
+//           skillCompetency: [{
+//             competencelevel: 5,
+//             skillNameUriPair:{
+//               '@name': cache.skillName,
+//               refURL: cache.skillRefUrl
+//             },
+//             weight: 1,
+//           }],
+//           selectionCriteria: 'Longest Available'
+//         }
+//       },
+//       prompt: {
+//         '@name': 'mainMenu.wav',
+//         refURL: 'https://uccx1.dcloud.cisco.com/adminapi/prompt/mainMenu.wav'
+//       }
+//     })
+//     .then(refURL => {
+//       console.log('created csq:', refURL)
+//       // extract csq ID
+//       cache.csqId = refURL.split('/').pop()
+//       done()
+//     })
+//     .catch(e => {
+//       done(e)
+//     })
+//   })
+// })
 
 describe('uccx.csq.list()', function () {
   it('should list csqs', function (done) {
     uccx.csq.list()
     .then(response => {
       console.log('found', response.length, 'csqs')
+      // console.log(response[47])
+      const c = response.find(v => v.name === 'Chat_4461')
+      console.log('found?:', c)
       done()
     })
     .catch(e => {
@@ -69,30 +76,30 @@ describe('uccx.csq.list()', function () {
   })
 })
 
-describe('uccx.csq.get(id)', function () {
-  it('should get csq', function (done) {
-    uccx.csq.get(cache.csqId)
-    .then(response => {
-      console.log('found CSQ', response.name)
-      // console.log(JSON.stringify(response, null, 2))
-      done()
-    })
-    .catch(e => {
-      done(e)
-    })
-  })
-})
+// describe('uccx.csq.get(id)', function () {
+//   it('should get csq', function (done) {
+//     uccx.csq.get(cache.csqId)
+//     .then(response => {
+//       console.log('found CSQ', response.name)
+//       // console.log(JSON.stringify(response, null, 2))
+//       done()
+//     })
+//     .catch(e => {
+//       done(e)
+//     })
+//   })
+// })
 
-// delete CSQ
-describe('uccx.csq.delete(id)', function () {
-  it('should delete csq', function (done) {
-    uccx.csq.delete(cache.csqId)
-    .then(response => {
-      console.log('deleted CSQ', cache.csqId)
-      done()
-    })
-    .catch(e => {
-      done(e)
-    })
-  })
-})
+// // delete CSQ
+// describe('uccx.csq.delete(id)', function () {
+//   it('should delete csq', function (done) {
+//     uccx.csq.delete(cache.csqId)
+//     .then(response => {
+//       console.log('deleted CSQ', cache.csqId)
+//       done()
+//     })
+//     .catch(e => {
+//       done(e)
+//     })
+//   })
+// })
